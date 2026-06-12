df3055d238212345d84690eef8bf50e8227d25e1
Sanskruti Raut <56341170+sanskruti-raut@users.noreply.github.com>
2026-06-12 07:51:29 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hashmap1 = {}
+        hashmap2 = {}
+        for i in range(len(s)):
+            if s[i] not in hashmap1:
+                hashmap1[s[i]] = 1
+            else:
+                hashmap1[s[i]] +=1
+                
+        for j in range(len(t)):
+            if t[j] not in hashmap2:
+                hashmap2[t[j]] = 1
+            else:
+                hashmap2[t[j]] +=1
+        
+        if hashmap1 == hashmap2:
+            return True
+        else:
+            return False
+
+        
